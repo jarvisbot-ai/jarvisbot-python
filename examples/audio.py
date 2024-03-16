bb03493e1d1adcf51968b395b8c91d391aac4e4a
Feixin2016 <xin.fei@fanttec.com>
2024-03-16 12:24:56 +0800

    Update audio.py

--- examples/audio.py
+++ examples/audio.py
@@ -8,8 +8,6 @@ speech_file_path = Path(__file__).parent / "speech.mp3"
 
 
 def main() -> None:
-    stream_to_speakers()
-
     # Create text-to-speech audio file
     with client.base.audio.speech.with_streaming_response.create(
             text="the quick brown fox jumped over the lazy dogs",
@@ -22,27 +20,6 @@ def main() -> None:
         input_format="mp3"
     )
     print(transcription.text)
-
-
-def stream_to_speakers() -> None:
-    import pyaudio
-
-    player_stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
-
-    start_time = time.time()
-
-    with client.base.audio.speech.with_streaming_response.create(
-            response_format="pcm",  # similar to WAV, but without a header chunk at the start.
-            text="""I see skies of blue and clouds of white
-                The bright blessed days, the dark sacred nights
-                And I think to myself
-                What a wonderful world""",
-    ) as response:
-        print(f"Time to first byte: {int((time.time() - start_time) * 1000)}ms")
-        for chunk in response.iter_bytes(chunk_size=1024):
-            player_stream.write(chunk)
-
-    print(f"Done in {int((time.time() - start_time) * 1000)}ms.")
 
 
 if __name__ == "__main__":
